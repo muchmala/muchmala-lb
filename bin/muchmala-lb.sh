0ac3e07be81f854fd805ba38602c1b77e6919157
George Miroshnykov <george.miroshnykov@gmail.com>
2011-07-09 12:10:48 +0300

    Using append instead of replace for daemon logging.

--- bin/muchmala-lb.sh
+++ bin/muchmala-lb.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # prepare for some BASH magic!
 LOGFILE="${MUCHMALA_LB_LOGDIR:-"/var/log/muchmala-lb"}"/daemon.log
-nohup ./bin/muchmala-lb.js "$@" > $LOGFILE < /dev/null 2>&1 &
+nohup ./bin/muchmala-lb.js "$@" >> $LOGFILE < /dev/null 2>&1 &
