4481966d4d90c8a5b3c46d13f6e5375468c565ee
George Miroshnykov <george.miroshnykov@gmail.com>
2011-07-10 18:43:25 +0300

    Create empty logdir if it's not there.

--- bin/muchmala-lb.sh
+++ bin/muchmala-lb.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-# prepare for some BASH magic!
-LOGFILE="${MUCHMALA_LB_LOGDIR:-"/var/log/muchmala-lb"}"/daemon.log
+MUCHMALA_LB_LOGDIR="${MUCHMALA_LB_LOGDIR:-"/var/log/muchmala-lb"}"
+if [ ! -d "$MUCHMALA_LB_LOGDIR" ]; then
+	mkdir -p "$MUCHMALA_LB_LOGDIR"
+fi
+
+LOGFILE="${MUCHMALA_LB_LOGDIR}"/daemon.log
 nohup ./bin/muchmala-lb.js "$@" >> $LOGFILE < /dev/null 2>&1 &
